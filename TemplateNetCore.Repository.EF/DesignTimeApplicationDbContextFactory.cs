c21ec2180efbbb61c4a362a418b6ab6012a8a0a3
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2021-06-19 16:45:02 -0300

    Update DesignTimeApplicationDbContextFactory.cs

--- TemplateNetCore.Repository.EF/DesignTimeApplicationDbContextFactory.cs
+++ TemplateNetCore.Repository.EF/DesignTimeApplicationDbContextFactory.cs
@@ -9,7 +9,7 @@ namespace TemplateNetCore.Repository.EF
         {
             var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
 
-            builder.UseNpgsql("Server=192.168.99.100;Port=5432;Database=template_net_core;User Id=postgres;Password=postgres;");
+            builder.UseSqlServer("");
 
             return new ApplicationDbContext(builder.Options);
         }
